16ca9ccd18646f7c6a600c4b6cb575deb321a86c
liao dick <liaodick@liaode-MacBook-Pro.local>
2020-12-07 09:30:00 +0800

    a1

---
+++ tty-2.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?
+echo $_POST["abc1"];
+
+
+?>
+</body>
+</html>
